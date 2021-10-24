84841b4eba4d17291f1b89db00a538eceb8f7c64
Arek Hulboj <baal_perez@wp.pl>
2021-10-24 20:06:17 +0200

    First commit

---
+++ src/test/java/com/kodilla/kodillacourse2/KodillaCourse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacourse2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaCourse2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
