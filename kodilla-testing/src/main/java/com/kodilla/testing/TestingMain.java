0bc43c450c1edad5a7a6193399b67792aedb4f6d
baalperez <baal_perez@wp.pl>
2021-10-26 18:28:30 +0200

    6.1.Added kodilla-testing module

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,7 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+    public static void main(String[] args) {
+        System.out.println("Modu³ 6. Wprowadzenie to testowania oprogramowania");
+    }
+}
