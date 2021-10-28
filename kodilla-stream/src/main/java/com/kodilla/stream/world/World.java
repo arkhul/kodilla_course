3a92552108a22136bab01a1e3066bedaa2be3c5b
baalperez <baal_perez@wp.pl>
2021-10-28 09:54:11 +0200

    7.4 testGetPeopleQuantity()

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -0,0 +1,23 @@
+package com.kodilla.stream.world;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class World {
+
+    private final Map<Continent, List<Country>> continentsWithCountries = new HashMap<>();
+
+    public void putEntryToMap(Continent continent, List<Country> theList) {
+        continentsWithCountries.put(continent, theList);
+    }
+
+    public BigDecimal getPeopleQuantity() {
+        BigDecimal finalResult = continentsWithCountries.entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream()
+                        .map(Country::getPeopleQuanity))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return finalResult;
+    }
+}
