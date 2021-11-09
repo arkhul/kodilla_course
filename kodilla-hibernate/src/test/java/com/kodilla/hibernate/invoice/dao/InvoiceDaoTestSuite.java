19ab2d5939fd7662522107b67456df0c8506483d
baalperez <baal_perez@wp.pl>
2021-11-09 10:58:12 +0100

    17.3 Final working app

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -18,17 +18,22 @@ public class InvoiceDaoTestSuite {
     private InvoiceDao invoiceDao;
     @Autowired
     private ProductDao productDao;
+    @Autowired
+    private ItemDao itemDao;
 
     @Test
     void testInvoiceDaoSave() {
         //Given
-        Item item1 = new Item(new BigDecimal("11.99"), 2, new BigDecimal("22.98"));
-        Item item2 = new Item(new BigDecimal("5.66"), 5, new BigDecimal("28.30"));
-
         Product potatoe = new Product("Potatoe");
         Product cucumber = new Product("Cucumber");
+        productDao.save(potatoe);
+        productDao.save(cucumber);
 
         Invoice invoice = new Invoice("fv/10/10/2051");
+        invoiceDao.save(invoice);
+
+        Item item1 = new Item(new BigDecimal("11.99"), 2, new BigDecimal("22.98"));
+        Item item2 = new Item(new BigDecimal("5.66"), 5, new BigDecimal("28.30"));
 
         invoice.setItem(item1);
         invoice.setItem(item2);
@@ -36,15 +41,23 @@ public class InvoiceDaoTestSuite {
         potatoe.setItem(item1);
         cucumber.setItem(item2);
 
-   //     invoiceDao.save(invoice);
-        productDao.save(potatoe);
+        itemDao.save(item1);
+        itemDao.save(item2);
 
         //When
+        int idInvoice = invoice.getId();
+        int idPotatoe = potatoe.getId();
+        int idItem1 = item1.getId();
 
         //Then
+        assertNotEquals(0, idInvoice);
+        assertNotEquals(0, idPotatoe);
+        assertNotEquals(0, idItem1);
 
         //CleanUp
+ //      itemDao.deleteAll();
  //      invoiceDao.deleteAll();
+ //      productDao.deleteAll();
 
     }
 }
